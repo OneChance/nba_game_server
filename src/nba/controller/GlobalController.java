9139351b8700155faf11618f20c769020b949e64
1 <1>
2015-03-12 20:53:58 +0800

    1

--- src/nba/controller/GlobalController.java
+++ src/nba/controller/GlobalController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/global")
+@RequestMapping("/")
 public class GlobalController {
 
 	@RequestMapping
